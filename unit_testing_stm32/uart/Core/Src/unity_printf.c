d40dbe4e3d25f1613b88b4dcf4808ba888c1deb7
lochu <lochu5vilehya@gmail.com>
2024-12-18 14:50:45 +0530

    unit testing stm32

---
+++ unit_testing_stm32/uart/Core/Src/unity_printf.c
@@ -0,0 +1,116 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "core_cm4.h"
+#include "unity.h"
+
+void SystemClock_Config(void);
+
+
+uint8_t captured_char;
+void setUp(){}
+void tearDown(){}
+
+void test_string_comparison(void)
+{
+    const char* expected_string = "Hello, Unity!";
+    const char* actual_string = "Hello, Unity!";  // This should match the expected string
+
+    // Compare strings
+    TEST_ASSERT_EQUAL_STRING(expected_string, actual_string);
+
+    // Test case where strings do not match
+    //const char* wrong_string = "Hello, World!";
+    //TEST_ASSERT_NOT_EQUAL_STRING(expected_string, wrong_string);  // This will fail
+}
+
+int main(void)
+{
+
+  HAL_Init();
+  SystemClock_Config();
+
+
+
+
+  while (1)
+  {
+	  UNITY_BEGIN();  // Initialize Unity Test framework
+	     printf("HIIIII\n");
+
+	     RUN_TEST(test_string_comparison);  // Run the string comparison test
+
+	     UNITY_END();
+
+  }
+  /* USER CODE END 3 */
+}
+
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+
+int _write(int file, char *ptr, int len)
+{
+  (void)file;
+  int DataIdx;
+
+  for (DataIdx = 0; DataIdx < len; DataIdx++)
+  {
+	  ITM_SendChar(*ptr++);
+  }
+  return len;
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
---
+++ unit_testing_stm32/uart/Debug/Core/Inc/unity.d
@@ -0,0 +1,4 @@
+Core/Inc/unity.o: ../Core/Inc/unity.c ../Core/Inc/unity.h \
+ ../Core/Inc/unity_internals.h
+../Core/Inc/unity.h:
+../Core/Inc/unity_internals.h:
