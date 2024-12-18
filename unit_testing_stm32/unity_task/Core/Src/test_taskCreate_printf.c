5d93fdfe4bf4c79fe44ce113e1af36701e2caa4f
lochu <lochu5vilehya@gmail.com>
2024-12-18 16:42:21 +0530

    unity test task create printf

---
+++ unit_testing_stm32/unity_task/Core/Src/test_taskCreate_printf.c
@@ -0,0 +1,166 @@
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "unity.h"
+
+UART_HandleTypeDef huart2;
+TaskHandle_t Task1Handle = NULL;
+
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void Task1_Handler(void *pvParameters);
+void test_GPIO_LED_Toggle(void);
+
+
+
+
+void setUp(void) {
+    // Setup code before each test, like resetting hardware or variables
+}
+
+void tearDown(void) {
+    // Cleanup code after each test
+}
+
+
+
+BaseType_t create_task(void) {
+    return xTaskCreate(Task1_Handler, "Task1", 128, NULL, 1, &Task1Handle);
+}
+
+void test_task_creation(void) {
+    BaseType_t result = create_task();
+    TEST_ASSERT_EQUAL(pdPASS, result); // Check if task creation was successful
+}
+
+
+int main(void)
+{
+
+
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+
+  UNITY_BEGIN();
+  RUN_TEST(test_task_creation); // Test task creation
+  UNITY_END();
+
+
+  printf("Starting project\n");
+  vTaskStartScheduler();
+
+
+  while (1)
+  {
+
+  }
+
+}
+
+void Task1_Handler(void *pvParameters)
+{
+    int count =0;
+        while(count < 10){
+            printf("hello,world : %d\n",count++);
+            vTaskDelay(500);
+        }
+}
+
+
+void vApplicationIdleHook(void)
+{
+}
+
+
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+int _write(int file, char *ptr, int len)
+{
+  (void)file;
+  int DataIdx;
+
+  for (DataIdx = 0; DataIdx < len; DataIdx++)
+  {
+      ITM_SendChar(*ptr++);
+  }
+  return len;
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+
